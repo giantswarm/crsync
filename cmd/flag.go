21877f2f7dc148b8ec55b7f82a8bac7fe8718a17
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-07-02 14:05:02 +0300

    Update cmd/flag.go
    Co-authored-by: Paweł Kopiczko <pawel@giantswarm.io>

--- cmd/flag.go
+++ cmd/flag.go
@@ -25,7 +25,7 @@ type flag struct {
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.DstRegistryName, flagDstRegistryName, "", `Destination container registry name. E.g.: "docker.io".`)
 	cmd.Flags().StringVar(&f.DstRegistryUser, flagDstRegistryUser, "", "Destination container registry user.")
-	cmd.Flags().StringVar(&f.DstRegistryPassword, flagDstRegistryPassword, os.Getenv(env.DstRegistryPassword), fmt.Sprintf(`Destination container registry password. Defaults to %s environment variable.`, env.DstRegistryPassword))
+	cmd.Flags().StringVar(&f.DstRegistryPassword, flagDstRegistryPassword, "", fmt.Sprintf(`Destination container registry password. Defaults to %s environment variable.`, env.DstRegistryPassword))
 }
 
 func (f *flag) Validate() error {
