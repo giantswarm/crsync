993b50d62d5678e551dfa810a8e8ffa9d9feab09
Paweł Kopiczko <kopiczko@gmail.com>
2020-07-14 10:41:05 +0100

    remove unused

--- pkg/registry/registry.go
+++ pkg/registry/registry.go
@@ -170,12 +170,3 @@ func executeCmd(binary string, args []string) error {
 
 	return nil
 }
-
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
