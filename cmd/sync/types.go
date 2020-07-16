dc8585c35533d9aca5e9f056f4a7def1ae287547
Paweł Kopiczko <kopiczko@gmail.com>
2020-07-16 12:28:00 +0100

    Prepare sync for parallel tags reading execution
    Towards https://github.com/giantswarm/giantswarm/issues/12061

--- cmd/sync/types.go
+++ cmd/sync/types.go
@@ -1,6 +1,16 @@
 package sync
 
-import "github.com/giantswarm/crsync/pkg/registry"
+import (
+	"github.com/giantswarm/crsync/pkg/registry"
+)
+
+type getTagsJob struct {
+	Src registry.Interface
+	Dst registry.Interface
+
+	ID   string
+	Repo string
+}
 
 type retagJob struct {
 	Src registry.Interface
