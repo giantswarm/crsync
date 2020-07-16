b9b8e44a31b87da11e19fbd8e1f8ca486a0d5eeb
Paweł Kopiczko <kopiczko@gmail.com>
2020-07-16 13:39:37 +0100

    revert extra rmi

--- cmd/sync/runner.go
+++ cmd/sync/runner.go
@@ -339,8 +339,6 @@ func (r *runner) processRetagJob(ctx context.Context, job retagJob) error {
 
 	err = registry.RetagImage(job.Repo, job.Tag, sourceRegistryName, r.flag.DstRegistryName)
 	if err != nil {
-		// Try to remove the image by best effort in case of error.
-		_ = job.Src.RemoveImage(ctx, job.Repo, job.Tag)
 		return microerror.Mask(err)
 	}
 
@@ -351,8 +349,6 @@ func (r *runner) processRetagJob(ctx context.Context, job retagJob) error {
 
 	err = job.Dst.Push(ctx, job.Repo, job.Tag)
 	if err != nil {
-		// Try to remove the image by best effort in case of error.
-		_ = job.Dst.RemoveImage(ctx, job.Repo, job.Tag)
 		return microerror.Mask(err)
 	}
 
