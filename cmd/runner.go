e473025d67878064a48799be9bf0c353ee4fa999
wtty-fool <wtty.fool@gmail.com>
2020-07-17 12:57:26 +0200

    serve prometheus metrics

--- cmd/runner.go
+++ cmd/runner.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spf13/cobra"
 
 	"github.com/giantswarm/crsync/pkg/project"
@@ -34,6 +37,22 @@ func (r *runner) Run(cmd *cobra.Command, args []string) error {
 	err := r.flag.Validate()
 	if err != nil {
 		return microerror.Mask(err)
+	}
+
+	if r.flag.MetricsPort != 0 {
+		go func() {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("serving metrics at :%d", r.flag.MetricsPort))
+			http.Handle("/metrics", promhttp.HandlerFor(
+				prometheus.DefaultGatherer,
+				promhttp.HandlerOpts{},
+			))
+			err := http.ListenAndServe(fmt.Sprintf(":%d", r.flag.MetricsPort), nil)
+			if err != nil {
+				r.logger.LogCtx(ctx, "level", "error", "message", "failed serving metrics", "stack", microerror.Mask(err))
+			}
+		}()
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "metrics disabled")
 	}
 
 	err = r.run(ctx, cmd, args)
