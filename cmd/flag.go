4e9b224cd9e670d55bf3aaebfb99cefeeaff8b53
wtty-fool <wtty.fool@gmail.com>
2020-07-17 12:57:18 +0200

    add metrics-port flag

--- cmd/flag.go
+++ cmd/flag.go
@@ -2,10 +2,16 @@ package cmd
 
 import "github.com/spf13/cobra"
 
+const (
+	flagMetricsPort = "metrics-port"
+)
+
 type flag struct {
+	MetricsPort int
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
+	cmd.Flags().IntVar(&f.MetricsPort, flagMetricsPort, 0, "Port on which metrics are served. 0 disables metrics.")
 }
 
 func (f *flag) Validate() error {
