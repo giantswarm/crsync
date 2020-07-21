b989474cdcb309b85e2c027f7806eea45abc1686
GitHub Action <action@github.com>
2020-07-21 14:23:22 +0000

    pkg/project: bump version to 0.5.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "crsync"
 	source      = "https://github.com/giantswarm/crsync"
-	version     = "0.5.0"
+	version     = "0.5.1-dev"
 )
 
 func Description() string {
