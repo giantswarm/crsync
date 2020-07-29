039f1ff37e1f68bf47c96c5f5a9a8fcf520271ff
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-07-29 15:51:59 +0300

    Add newline

--- cmd/sync/runner.go
+++ cmd/sync/runner.go
@@ -160,7 +160,7 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 
 	if r.flag.MetricsPort != 0 {
 		go func() {
-			fmt.Printf("Serving metrics at :%d", r.flag.MetricsPort)
+			fmt.Printf("Serving metrics at :%d\n", r.flag.MetricsPort)
 			http.Handle("/metrics", promhttp.HandlerFor(
 				prometheus.DefaultGatherer,
 				promhttp.HandlerOpts{},
