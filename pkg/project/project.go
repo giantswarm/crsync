21c1afdf5f11470647d4eb2720ec0ce554328422
GitHub Action <action@github.com>
2022-02-24 11:46:01 +0000

    Bump version to 0.6.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "crsync"
 	source      = "https://github.com/giantswarm/crsync"
-	version     = "0.6.1"
+	version     = "0.6.2-dev"
 )
 
 func Description() string {
