e7585e986aed8a1886e9b15d09e420a816f28289
taylorbot <dev@giantswarm.io>
2023-11-28 15:06:06 +0000

    Release v0.8.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "crsync"
 	source      = "https://github.com/giantswarm/crsync"
-	version     = "0.7.1-dev"
+	version     = "0.8.0"
 )
 
 func Description() string {
