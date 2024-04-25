aa4aeef3a5c5cfc738d152d0bf64568620ac07e9
taylorbot <dev@giantswarm.io>
2024-04-25 10:52:21 +0000

    Release v0.10.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "crsync"
 	source      = "https://github.com/giantswarm/crsync"
-	version     = "0.9.4"
+	version     = "0.10.0"
 )
 
 func Description() string {
